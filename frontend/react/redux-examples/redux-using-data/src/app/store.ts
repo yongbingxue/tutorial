28229e42e38c348f192caa7a5c6913c0dc815e26
Xue, Yongbing <yongbingxue@163.com>
2024-12-31 10:00:17 +0800

    move react to frontend

---
+++ frontend/react/redux-examples/redux-using-data/src/app/store.ts
@@ -0,0 +1,20 @@
+import { configureStore } from '@reduxjs/toolkit'
+
+import authReducer from '@/features/auth/authSlice'
+import postsReducer from '@/features/posts/postsSlice'
+import usersReducer from '@/features/users/usersSlice'
+
+export const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    posts: postsReducer,
+    users: usersReducer,
+  },
+})
+
+// Infer the type of `store`
+export type AppStore = typeof store
+// Infer the `AppDispatch` type from the store itself
+export type AppDispatch = typeof store.dispatch
+// Same for the `RootState` type
+export type RootState = ReturnType<typeof store.getState>
